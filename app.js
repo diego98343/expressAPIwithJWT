2ed2fa580b2afe8492a737e40915d7cb0da5f704
diegocolonia <diegocolonia98@yahoo.com>
2023-03-29 15:44:22 -0400

    commit

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-
+//connectDB
+const connectDB= require('./db/connect')
+//routers imports
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+
 
 app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/auth', authRouter);
+app.use('api/jobs', jobsRouter);
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,6 +29,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    //connect to dabaBase
+     await connectDB(process.env.MANGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
