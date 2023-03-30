0642a62219d5e6e83980c45b4f7792725fb3ec5c
diegocolonia <diegocolonia98@yahoo.com>
2023-03-30 01:44:45 -0400

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,19 +2,23 @@ const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes');
 const { BadRequestError } = require('../errors');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const register = async (req, res) => {
-   const { name, email, password } = req.body
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password,salt)
-   
-   const tempUser ={name,email,password:hashedPassword}
-
+    
+    const { name, email, password } = req.body
+//    const salt = await bcrypt.genSalt(10);
+//    const hashedPassword = await bcrypt.hash(password,salt)
+//    const tempUser ={name,email,password:hashedPassword}
     if (!name || !email || !password) {
         throw new BadRequestError('please provide name, email and password');
     }
-    const user = await User.create({ ...tempUser });
-    res.status(StatusCodes.CREATED).json({ user });
+
+    const user = await User.create({ ...req.body });
+
+    const token = jwt.sign({ userId: user._id },'jwtSecret',{expiresIn:'30d'});
+
+    res.status(StatusCodes.CREATED).json({ user:{name:user.name},token });
 }
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -17,10 +18,15 @@ const UserSchema = new mongoose.Schema({
      password: {
         type: String,
         required: [true, 'please provide a name'],
-        minlength: 6,
-        
+        minlength: 6,  
     },
     
+})
+
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 })
 
 module.exports = mongoose.model('User',UserSchema)
