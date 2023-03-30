a6b8cf82c726d93167f008744cf9442f9d80eb5c
diegocolonia <diegocolonia98@yahoo.com>
2023-03-30 02:00:15 -0400

    commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -16,9 +16,9 @@ const register = async (req, res) => {
 
     const user = await User.create({ ...req.body });
 
-    const token = jwt.sign({ userId: user._id },'jwtSecret',{expiresIn:'30d'});
+    const token = user.createJWT();
 
-    res.status(StatusCodes.CREATED).json({ user:{name:user.name},token });
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 }
 
 const login = async (req, res) => {
